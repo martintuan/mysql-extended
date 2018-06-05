475db206db657694b3b615e9e1852528415b6661
Nguyen <Martin.Nguyen@wolterskluwer.com>
2018-06-05 17:10:13 -0400

    modify init

--- init_db.sh
+++ init_db.sh
@@ -1,6 +1,6 @@
  #!/bin/bash 
  #/usr/bin/mysqld_safe & sleep 5 
  sleep 10
- mysql -u root -e "CREATE DATABASE crlonline;" 
- mysql -u root crlonline < /tmp/test.sql 
+ mysql -uroot -pmysql -e "CREATE DATABASE crlonline;" 
+ mysql -uroot -pmysql crlonline < /tmp/test.sql 
 
